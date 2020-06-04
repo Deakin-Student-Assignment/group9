d124c84cd7ae07973aa8c59165453efb64a86d21
Connie C Cox <cloud.launchpad@icloud.com>
2020-06-04 14:18:20 +1000

    update of env.js

--- public/env.js
+++ public/env.js
@@ -1,14 +1,3 @@
  $(document).ready(function () {
-     var label = 'What is your name?'
-     $('#lblName').text(label)
 
-     // bind submit button
-     $('#submit').click(function () {
-
-         var name = $('#name').val()
-
-
-         $('#lblName').val('')
-         $('#name').val("Hello " + name)
-     })
  });
