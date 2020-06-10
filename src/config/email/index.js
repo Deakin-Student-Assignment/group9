502b76f8d21bd3de05cf135286aba172c6b75148
Connie C Cox <cloud.launchpad@icloud.com>
2020-06-10 16:49:49 +1000

    update branch

---
+++ src/config/email/index.js
@@ -0,0 +1,20 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+require("dotenv").config();
+
+
+
+module.exports.sendEmail = function (request, callback) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const msg = {
+        to: request,
+        from: 'no-reply@book-a-car.com',
+        subject: 'Confirmation Email',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+    };
+
+    sgMail.send(msg);
+};
