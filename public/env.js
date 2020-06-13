38144fffb6cafaf32f8e82acfed676f4762b5a59
Connie C Cox <cloud.launchpad@icloud.com>
2020-06-13 22:41:53 +1000

    update feature branch

--- public/env.js
+++ public/env.js
@@ -1,103 +1,118 @@
- $(document).ready(function () {
+$(document).ready(function () {
+    var deladdress, deldate, delreturn;
 
-     // bind the nav bar
-     $('.sidenav').sidenav();
+    // bind the nav bar
+    $('.sidenav').sidenav();
+
+    // bind date pickers
+    $('#dpDelivery').datepicker({
+        format: "dd-mm-yyyy"
+    });
+    $('#dpReturn').datepicker({
+        format: "dd-mm-yyyy"
+    });
+
+    // bind the search button
+    $('#btnSearch').click(function () {
+        doSearch();
+    })
+
+    // bind the login button
+    $('#btnLogin').click(function () {
+        doLogin();
+    })
+
+    // bind the sign up button
+    $('#signup').submit(function (event) {
+        event.preventDefault();
+        doSignup();
+    })
+
+    $('#cars').collapsible();
+
+    function doSignup() {
+        var user = {
+            _id: $('#email').val(),
+            user: {
+                first_name: $('#first_name').val(),
+                surname: $('#surname').val(),
+                email: $('#email').val(),
+                password: $('#password').val()
+            }
+        }
+
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            url: "./signup",
+            data: JSON.stringify(user),
+            dataType: 'json',
+            success: function (message) {
+                console.log(message);
+                var x = document.getElementById("successsign");
+                var y = document.getElementById("signup");
+
+                if (x.style.display === "none") {
+                    x.style.display = "block";
+                    y.style.display = "none";
+                } else {
+                    x.style.display = "none";
+                }
+            },
+            error: function (e) {
+                alert("Error");
+                console.log(e);
+            }
+        });
+
+        resetForm();
+    }
+
+    function resetForm() {
+        $('#first_name').val("");
+        $('#surname').val("");
+        $('#email').val("");
+        $('#password').val("");
+    }
+
+    function doSearch() {
+
+        var searchq = {
+            location: $('#address').val(),
+            deldate: $('#dpDelivery').datepicker().val(),
+            delreturn: $('#dpReturn').datepicker().val()
+        };
+
+        //console.log(searchq);
+        deladdress = $('#address').val();
+        deldate = $('#dpDelivery').datepicker().val();
+        delreturn = $('#dpReturn').datepicker().val();
+        localStorage.setItem('deladdress', deladdress);
+        localStorage.setItem('deldate', deldate);
+        localStorage.setItem('delreturn', delreturn);
 
 
-     // bind date pickers
-     $('.datepicker').datepicker();
-
-     // bind the search button
-     $('#btnSearch').click(function () {
-
-         doSearch();
-     })
-
-     $('#btnLogin').click(function () {
-         doLogin();
-     })
-
-     /* $('#btnSignup').click(function () {
-          var url = "./signup/"
-          var fname = $('#first_name').val();
-          var sname = $('#surname').val();
-          var e = $('#email').val();
-          var pwd = $('#password').val();
-
-          var user = {
-              _id: e,
-              user: {
-                  first_name: fname,
-                  surname: sname,
-                  email: e,
-                  password: pwd
-              }
-          };
-
-          $.post(url, user, function (data) {
-              console.log("user is " + JSON.stringify(user));
-              console.log(data);
-          });
-      })*/
-     $('#signup').submit(function (event) {
-         event.preventDefault();
-         doSignup();
-     })
-
-     function doSignup() {
-         var user = {
-             _id: $('#email').val(),
-             user: {
-                 first_name: $('#first_name').val(),
-                 surname: $('#surname').val(),
-                 email: $('#email').val(),
-                 password: $('#password').val()
-             }
-         }
-
-         $.ajax({
-             type: "POST",
-             contentType: "application/json",
-             url: "./signup",
-             data: JSON.stringify(user),
-             dataType: 'json',
-             success: function (message) {
-                 console.log(message);
-                 var x = document.getElementById("successsign");
-                 var y = document.getElementById("signup");
-
-                 if (x.style.display === "none") {
-                     x.style.display = "block";
-                     y.style.display = "none";
-                 } else {
-                     x.style.display = "none";
-                 }
-             },
-             error: function (e) {
-                 alert("Error");
-                 console.log(e);
-             }
-         });
-
-         resetForm();
-     }
-
-     function resetForm() {
-         $('#first_name').val("");
-         $('#surname').val("");
-         $('#email').val("");
-         $('#password').val("");
-     }
+        $(location).attr('href', './reservation.html');
+    };
 
 
- })
 
+})
 
- function doSearch() {
-     alert('search button clicked');
+function doLogin() {
+    alert('login button clicked');
+}
 
- }
+function getStorage() {
+    var deladd, deldate, delreturn;
 
- function doLogin() {
-     alert('login button clicked');
- }
+    deladd = localStorage.getItem('deladdress');
+    localStorage.removeItem('deladdress');
+    deldate = localStorage.getItem('deldate');
+    localStorage.removeItem('deldate');
+    delreturn = localStorage.getItem('delreturn');
+    localStorage.removeItem('delreturn');
+    $('#deladdress').val(deladd);
+    $('#deldate').val(deldate);
+    $('#delreturn').val(delreturn);
+};
